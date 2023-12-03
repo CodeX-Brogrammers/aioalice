41cc8efd98ad0fffac7da2ceb5a922a1635047b6
Demetrous-fd <dimaprodeus@yandex.ru>
2023-12-03 12:10:53 +0300

    Update ensure_cls from utils for many types input

--- aioalice/types/image.py
+++ aioalice/types/image.py
@@ -1,6 +1,7 @@
 from attr import attrs, attrib
 
-from aioalice.types import AliceObject, MediaButton
+
+from aioalice.types import AliceObject, MediaButton, Button
 from aioalice.utils import ensure_cls
 
 
@@ -10,4 +11,4 @@ class Image(AliceObject):
     image_id = attrib(type=str)
     title = attrib(type=str)
     description = attrib(type=str)
-    button = attrib(default=None, converter=ensure_cls(MediaButton))
+    button = attrib(default=None, converter=ensure_cls(Button, MediaButton))
--- aioalice/utils/__init__.py
+++ aioalice/utils/__init__.py
@@ -4,20 +4,30 @@ from aioalice.utils.payload import generate_json_payload
 from aioalice.utils.safe_kwargs import safe_kwargs
 
 
-def ensure_cls(klass):
+def ensure_cls(*classes):
     from aioalice.types.base import AliceObject
-    safe_cls = safe_kwargs(klass) if issubclass(klass, AliceObject) else klass
+
+    safe_classes = [safe_kwargs(cls) if issubclass(cls, AliceObject) else cls for cls in classes]
 
     def converter(val):
         if val is None:
             return
         if isinstance(val, dict):
-            return safe_cls(**val)
+            for cls in safe_classes:
+                try:
+                    return cls(**val)
+                except TypeError:
+                    pass
         if isinstance(val, list):
             return [converter(v) for v in val]
-        if not isinstance(val, klass):
-            return klass(val)
+        for cls in classes:
+            if not isinstance(val, cls):
+                try:
+                    return cls(val)
+                except TypeError:
+                    pass
         return val
+
     return converter
 
 
